bf351a2abf395a530645131b28ca427bbe07f179
unknown <josephlawler23@utexas.edu>
2020-02-13 18:16:05 -0600

    Wrote dimmer function

--- examples/Lab1_Blinky/Lab1_Blinky.ino
+++ examples/Lab1_Blinky/Lab1_Blinky.ino
@@ -12,10 +12,26 @@ void timedBlink(int interval){
   digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
   delay(interval);                       // wait for interval seconds
 }
+void dimmer(int freq, int duty) {
+  int period, onTime, offTime;
+  period = 1000/freq;
+  onTime = period * duty / 100;
+  offTime = period - onTime;
+  digitalWrite(LED_PIN, HIGH);
+  delay(onTime);
+  digitalWrite(LED_PIN, LOW);
+  delay(offTime);
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
-  timedBlink(250);
-  timedBlink(500);
-  timedBlink(1000);
+  for(int i = 0; i < 100; i = i + 10){
+    dimmer(100-i,i);
+  }
+  for(int i = 0; i < 100; i = i + 10){
+    dimmer(100-i,100-i);
+  }
+//  timedBlink(250);
+//  timedBlink(500);
+//  timedBlink(1000);
 }
