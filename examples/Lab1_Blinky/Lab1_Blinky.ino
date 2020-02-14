a4e7fc2462b0c17806a15dcd0fb18c453ec7fe0f
unknown <josephlawler23@utexas.edu>
2020-02-13 18:08:05 -0600

    Wrote timedBlink function

--- examples/Lab1_Blinky/Lab1_Blinky.ino
+++ examples/Lab1_Blinky/Lab1_Blinky.ino
@@ -1,9 +1,21 @@
+#define LED_PIN 2
+
 void setup() {
   // put your setup code here, to run once:
+  // initialize digital pin LED_PIN as an output.
+  pinMode(LED_PIN, OUTPUT);
+}
 
+void timedBlink(int interval){
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(interval);                       // wait for interval seconds
+  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(interval);                       // wait for interval seconds
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  timedBlink(250);
+  timedBlink(500);
+  timedBlink(1000);
 }
